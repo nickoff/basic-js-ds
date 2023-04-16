25e679454918896f0224314362d4340ccc8ae335
Nickoff <v3136820@gmail.com>
2023-04-16 19:09:42 +0300

    feat: comleted: stack (Sun, Apr 16, 2023 07:09:41 PM)

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  push(element) {
+    if(!this.array) {
+      this.array = [] 
+    }
+    this.array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.array) {
+      return this.array.pop();
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.array) {
+      return this.array[this.array.length - 1];
+    }
   }
 }
 
